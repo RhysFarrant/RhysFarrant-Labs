4aa7a2ab3460228b074405dda3cf1274878b5542
Rhys Farrant <FarrantRhys@gmail.com>
2026-02-09 23:24:19 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>RhysFarrant-Labs</h1>
+      <p>React + Vite starter placeholder app.</p>
+    </main>
+  );
+}
+
+export default App;
